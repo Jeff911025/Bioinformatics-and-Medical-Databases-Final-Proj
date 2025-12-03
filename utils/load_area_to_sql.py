2426ad4ad2f562d411ae528528588d256ab36e8b
unknown <jeff0102030433@gmail.com>
2025-12-03 20:04:16 +0800

    update

--- utils/load_area_to_sql.py
+++ utils/load_area_to_sql.py
@@ -1,2 +1,10 @@
 # animal_area_pkid(動物所屬縣市代碼)
 # 
+import pandas as pd
+import sqlite3
+
+if __name__ == "__main__":
+    csv_path = "COA_OpenData.csv"
+    db_path = "test.db"
+    df = pd.read_csv(csv_path, encoding="utf-8")
+    con = sqlite3.connect(db_path)
